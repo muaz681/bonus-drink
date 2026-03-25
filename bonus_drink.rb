29afb6d9e0fdc95d619a5f98c02de9ed8f0e5b4d
Mowaz Ahmad <50701163+muaz681@users.noreply.github.com>
2026-03-26 01:00:27 +0900

    Solve problem
    Implement logic to calculate total bonus drinks based on the amount provided.

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = amount
+    empty = amount
+
+    while empty >= 3
+      new_drinks = empty / 3
+      total += new_drinks
+      empty = new_drinks + (empty % 3)
+    end
+    return total
   end
 end
